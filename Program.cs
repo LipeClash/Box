bee89557035d786c0424da164d6f97e318367ae1
LipeClash <felipeclaudino2003@hotmail.com>
2020-05-10 17:23:43 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace auladepc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+--------+");
+            Console.WriteLine("| Felipe |");
+            Console.WriteLine("+--------+");
+        }
+    }
+}
